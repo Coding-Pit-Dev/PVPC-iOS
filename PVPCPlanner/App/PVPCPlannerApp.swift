fe61d47fb69cda87cd0da5b2a2619a89048baadc
Oliver R.C <oliverrc83@me.com>
2024-10-07 11:39:20 +0200

    [Add]: Firebase to proyect and Allow Arbitrary Loads to info.plist

--- PVPCPlanner/App/PVPCPlannerApp.swift
+++ PVPCPlanner/App/PVPCPlannerApp.swift
@@ -2,6 +2,9 @@ import SwiftUI
 
 @main
 struct PVPCPlannerApp: App {
+    // register app delegate for Firebase setup
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+
     var body: some Scene {
         WindowGroup {
             PricesView()
