f9cf1215158397dd5d05021813697089de3b1f5f
mriaus <55255215+mriaus@users.noreply.github.com>
2024-10-25 15:28:51 +0200

    feature: Added the CustomLazyList component and solved a warning in PVPCDatabaseContainer

---
+++ PVPCPlanner/View/Components/CustomLazyList.swift
@@ -0,0 +1,70 @@
+import SwiftUI
+
+enum CustomLazyListDirection {
+    case vertical
+    case horizontal
+}
+
+struct CustomLazyList<Content: View>: View {
+    var spacing: CGFloat
+    var listDirection: CustomLazyListDirection
+    var backgroundColor: Color
+    var content: () -> Content
+
+    init(spacing: CGFloat = 10,
+         listDirection: CustomLazyListDirection = CustomLazyListDirection.vertical,
+         backgroundColor: Color = Color.white,
+         @ViewBuilder content: @escaping () -> Content) {
+
+        self.spacing = spacing
+        self.listDirection = listDirection
+        self.backgroundColor = backgroundColor
+        self.content = content
+    }
+
+    var body: some View {
+        ScrollView {
+            Group {
+                if listDirection == .vertical {
+                    LazyVStack(spacing: spacing) {
+                        content()
+                    }
+                    .padding()
+                } else {
+                    LazyHStack(spacing: spacing) {
+                        content()
+                    }
+                    .padding()
+                }
+            }.background(backgroundColor)
+        }
+    }
+}
+
+// Example with Divider and custom settings
+#Preview {
+    CustomLazyList(spacing: 30, listDirection: .vertical, backgroundColor: Color.cyan) {
+        ForEach(0 ..< 10, id: \.self) { index in
+            Text("Item \(index + 1)")
+                .padding()
+                .background(Color.gray.opacity(0.2))
+                .cornerRadius(8)
+
+            if index < 9 {
+                Divider()
+            }
+        }
+    }
+}
+
+// Example with defaults
+#Preview {
+    CustomLazyList {
+        ForEach(0 ..< 10, id: \.self) { index in
+            Text("Item \(index + 1)")
+                .padding()
+                .background(Color.gray.opacity(0.2))
+                .cornerRadius(8)
+        }
+    }
+}
