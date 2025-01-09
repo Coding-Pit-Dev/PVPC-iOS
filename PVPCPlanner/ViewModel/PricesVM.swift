ffb016c679d456041d12bb960d29f920148ddf48
Oliver R.C <oliverrc83@me.com>
2025-01-09 18:07:04 +0100

    [Refactor]: swift yaml of test. WIP refactor code

--- PVPCPlanner/ViewModel/PricesVM.swift
+++ PVPCPlanner/ViewModel/PricesVM.swift
@@ -15,7 +15,6 @@ final class PricesVM {
     var errorMsg = ""
     var showError = false
 
-    @MainActor
     init(getPricesUseCase: PricesUseCaseProtocol = GetPricesUseCase.shared,
          addPVPCTOLocalDBUseCase: AddToLocalDBUseCaseProtocol = AddPVPCToLocaDBUseCase.shared,
          getPVPCByDayFromLocalDBUseCase: GetByDayFromDBUseCaseProtocol = GetPVPCByDayFromLocalDBUseCase()
