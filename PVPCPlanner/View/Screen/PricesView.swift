91bc96b37d0a0ca8ceff54777d458456996e56f1
Oliver R.C <oliverrc83@me.com>
2025-01-16 19:19:48 +0100

    [Refactor]: Comeback old file swift yaml + pricesVW change to github action

--- PVPCPlanner/View/Screen/PricesView.swift
+++ PVPCPlanner/View/Screen/PricesView.swift
@@ -2,18 +2,27 @@ import SwiftUI
 
 struct PricesView: View {
 
-    @State private var priceViewModel: PricesVM = PricesVM()
+    // @State private var priceViewModel: PricesVM = PricesVM()
+    @State private var priceViewModel: PricesVM?
     @AppStorage(AppStorageKeys.LOCATION.rawValue) var selectedLocation: Locations = .MainlandAndIslands
 
     var body: some View {
         VStack {
             CustomLazyList(listDirection: .vertical, backgroundColor: Color.clear) {
+                if let priceViewModel = priceViewModel {
                     ForEach(priceViewModel.prices, id: \.self) { price in
                         PricesCard(pvpcCardModel: price.toPVPCCardModel(location: selectedLocation))
                     }
+                }
             }
             .task {
-                await priceViewModel.setPrices()
+                await MainActor.run {
+                    self.priceViewModel = PricesVM()
+                }
+
+                if let priceViewModel = priceViewModel {
+                    await priceViewModel.setPrices()
+                }
             }
         }
     }
